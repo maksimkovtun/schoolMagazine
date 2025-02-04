462ea01ea2e34fb534d6ee36d004a561bae99945
maksimkovtun <makskovtun2002@mail.ru>
2025-02-04 14:23:08 +0700

    .
    .

---
+++ src/main/java/com/project/schoolmagazine/SchoolMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.project.schoolmagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMagazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/schoolmagazine/SchoolMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.schoolmagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
